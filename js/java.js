91ef01f2389bc34c5f46ba83e961b77c8f643d76
Sangir-08 <sangir.pyurbeev@yandex.ru>
2021-05-31 17:20:40 +0300

    add my diplom

---
+++ js/java.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+	setTimeout(() => {
+	  $(".splash").addClass("display-none");
+	  setTimeout(() => {
+		$(".splash").css("display", "none");
+	  }, 2000);
+	}, 1500);
+
+
+	$('.single-item').slick({
+		autoplay: true,
+  		autoplaySpeed: 3000,
+		// infinite: true,
+		fade: true,
+		dots: true,
+		// slidesToShow: 1,
+		// slidesToScroll: 1
+	  });
+
+	$('.slider-games').slick({
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		arrows: false
+	  });
+
+
+
+  });
